bcd87393d92fc31699eb447724ad511d7de70313
NFT2626 <96617076+NFT2626@users.noreply.github.com>
2022-04-08 22:24:22 +0000

    Update LearnMore.js

--- src/Components/LearnMore/LearnMore.js
+++ src/Components/LearnMore/LearnMore.js
@@ -26,7 +26,7 @@ const Wrapper = styled.div`
   }
   .image {
     width: 100%;
-    height: 400px;
+    height: 300px;
     object-fit: cover;
     border-radius: 15px;
   }
@@ -142,6 +142,7 @@ const LearnMore = () => {
   return (
     <Col className="mx-auto " id="learnmore" lg={11} xl={10} xxl={7}>
       <Wrapper>
+        <img src="./images/AlternativeSea2.png" alt="" className="image" />
         <h1 className="subtitle">
           Every time you save in game your world is synced to the blockchain and powered by IPFS. Build with
           freedom, with no fear of censorship or loss of build data. Own a plot now build a community 
