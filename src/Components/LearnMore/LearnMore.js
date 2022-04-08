186bd7ab862279f359447544033aec4ba6097f12
NFT2626 <96617076+NFT2626@users.noreply.github.com>
2022-04-08 22:18:43 +0000

    Update LearnMore.js

--- src/Components/LearnMore/LearnMore.js
+++ src/Components/LearnMore/LearnMore.js
@@ -26,9 +26,9 @@ const Wrapper = styled.div`
   }
   .image {
     width: 100%;
-    height: 300px;
+    height: 400px;
     object-fit: cover;
-    border-radius: 25px;
+    border-radius: 15px;
   }
   .title {
     font-size: 35px;
@@ -107,8 +107,8 @@ const LearnMore = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 2,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     initialSlide: 0,
     swipeToSlide: true,
     responsive: [
